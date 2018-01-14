b8f89c077a0c9fe1091c8f171102af16a58705ad
Tal Rasha <talrasha007@gmail.com>
2018-01-14 20:16:22 +0800

    font awsome.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+Vue.component('icon', Icon);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +16,4 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
-})
+});
