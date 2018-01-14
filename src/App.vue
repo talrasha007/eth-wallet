bac87f6c91b0206bff756d0ad0821c248550fb5b
Tal Rasha <talrasha007@gmail.com>
2018-01-14 21:33:39 +0800

    nav style.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <ul class="header clearfix">
+      <li v-bind:class="{ active: $route.path === '/' }"><a href="#/"><icon name="credit-card" class="icon" /></a>钱包</li>
+      <li v-bind:class="{ active: $route.path === '/token' }"><a href="#/token"><icon name="gg" class="icon" /></a>代币</li>
+    </ul>
     <router-view />
   </div>
 </template>
@@ -12,12 +15,56 @@ export default {
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+</style>
+
+<style scoped>
+ul.header {
+  padding: 17px 80px 0 80px;
+  background: #f0f0f0;
+  margin: 0;
+  list-style: none;
+}
+
+ul.header li {
+  color: #0285c0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0 30px;
+  float: left;
+  border-bottom: 5px solid #f0f0f0;
+}
+
+ul.header li a {
+  color: inherit;
+}
+
+ul.header li a:hover {
+  cursor: pointer;
+}
+
+ul.header li.active {
+  color: #b7a6a6;
+  border-bottom: 5px solid white;
+}
+
+ul.header li .icon {
+  width: 32px;
+  height: 32px;
+  display: block;
 }
 </style>
