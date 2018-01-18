213708e4a9c46b0180bc286c7a813bffba5ec410
Tal Rasha <talrasha007@gmail.com>
2018-01-18 20:45:18 +0800

    Error page.

---
+++ src/components/Error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content">
+    <h1>木有安装MetaMask？</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "error"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Wallet from '@/components/Wallet'
+import Error from '@/components/Error'
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Wallet',
       component: Wallet
+    },
+    {
+      path: '/error',
+      name: 'Error',
+      component: Error
     }
   ]
 })
