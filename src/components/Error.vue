2f4d6bf9a2f3e920361e22c300d4051d2a18ce05
ttl <talrasha007@gmail.com>
2018-01-19 18:57:08 +0800

    update error page.

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="content">
-    <h1>木有安装MetaMask？</h1>
+  <div class="content" v-if="!metamaskInstalled">
+    <h1>根据我多年的经验，你木有安装MetaMask？</h1>
+    <a href="https://metamask.io" target="_blank">
+      <img src="../assets/download-metamask.png" />
+    </a>
+  </div>
+  <div class="content" v-else-if="locked">
+    <h1>快去解锁你的MetaMask吧</h1>
+  </div>
+  <div class="content" v-else>
+    <h1>我是谁？我为什么会在这里？</h1>
   </div>
 </template>
 
 <script>
   export default {
-    name: "error"
+    name: "error",
+    data: () => ({
+      metamaskInstalled: !!window.web3,
+      locked: window.web3.eth.accounts.length === 0
+    })
   }
 </script>
 
