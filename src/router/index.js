bba42bbc93d9262d62a085ddca72aecdca7e717d
ttl <talrasha007@gmail.com>
2018-02-01 15:58:26 +0800

    quotation route.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Wallet from '@/components/Wallet'
 import TTL from '@/components/TTL'
+import Quotation from '@/components/Quotation'
 import Error from '@/components/Error'
 
 Vue.use(Router);
@@ -21,7 +22,13 @@ export default new Router({
       path: '/ttl',
       name: 'ttl',
       component: TTL
-    },    {
+    },
+    {
+      path: '/quotation',
+      name: 'quotation',
+      component: Quotation
+    },
+    {
       path: '/error',
       name: 'error',
       component: Error
